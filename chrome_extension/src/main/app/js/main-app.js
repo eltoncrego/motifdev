0b768c830220364a797a27cbbf400c8752413141
Elton C. Rego <rego.elton@gmail.com>
2019-10-30 20:56:51 -0700

    M-6 : Replace the spotify icon with our icon

--- chrome_extension/src/main/app/js/main-app.js
+++ chrome_extension/src/main/app/js/main-app.js
@@ -1,16 +1,6 @@
-$(async function() { 
-    await changeSpotify();
-});
-
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, 3000));
-}
-
-async function changeSpotify() {
-    // Need to wait for spotifies dom structure to load... happens through react so it
-    // may not available when this script gets originally run on the document.ready
-    // event... we can probably do a better check by doing shorter polls
-    // and retrying every 500 ms or something like that until we find
-    // some element we are looking for that is guaranteed to be there eventually
-    sleep(3000).then(r => $(".tracklist").children().append("<p>hey ;)</p>"));
-}
+console.log("Motif for Spotify is running!");
+var ourSVGURL = chrome.extension.getURL("../../../../assets/img/main-logo.svg");
+$.get(ourSVGURL, function(response) {
+  var spotifySVG = document.getElementsByClassName('spotify-logo--text');
+  spotifySVG[0].parentElement.innerHTML = response;
+}, 'html');
