deff467e64f332a01ef689a3520102357bf51c61
harrisonroth <harrison@gotroth.com>
2019-10-30 20:06:22 -0700

    M-5: finish first attempt on tagged song schema

--- server/models/taggedSong.js
+++ server/models/taggedSong.js
@@ -4,9 +4,11 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
  
-const taggedSong = new Schema({
+const taggedSongSchema = new Schema({
   	user: String,
 	title: String,
 	id: String
   	tags: [String]
 });
+
+const taggedSong = mongoose.model('taggedSong', taggedSongSchema);
