470255a7d8b29b342b3384e98f95b71898acb19f
harrisonroth <harrison@gotroth.com>
2019-11-01 07:01:32 -0700

    M-5: structure server files

--- server/app.js
+++ server/app.js
@@ -1,11 +1,44 @@
-// app.js
-const express = require('express')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Create Express app
-const app = express()
+var indexRouter = require('./routes/index');
+var songsRouter = require('./routes/songs');
 
-// A sample route
-app.get('/', (req, res) => res.send('Hello World!'))
+var db = require('./controllers/db');
 
-// Start the Express server
-app.listen(3000, () => console.log('Server running on port 3000!'))
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/songs', songsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(3000);
+
+module.exports = app;
--- server/mongo.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
- 
-mongoose.connect('mongodb://localhost/my_database', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
