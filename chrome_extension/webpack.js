0e9024821ef0018a8e9304965541a384218e876b
Alex Jackson <ajjackson91195@gmail.com>
2019-11-05 22:47:08 -0800

    M-25 Add some missing linting files, add babel-loader to support some es6 features

--- chrome_extension/webpack.js
+++ chrome_extension/webpack.js
@@ -45,11 +45,11 @@ const config = {
   ],
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: ["babel-loader"]
-      // },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
